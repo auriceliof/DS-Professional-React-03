e9f48db1b2bfbd36dcef314a5dca1b102e245fab
auriceliof <auricelio.suporte@hotmail.com>
2023-12-26 22:23:32 -0300

    03-1 Adicionando primeiros links

--- tutorial-react-router/src/App.tsx
+++ tutorial-react-router/src/App.tsx
@@ -1,8 +1,18 @@
 import './App.css'
+import { Link } from 'react-router-dom';
 
 export default function App() {
 
   return (
-    <h1>BookKeeper!</h1>
+    <nav
+      style={{
+        borderBottom: "solid 1px",
+        paddingBottom: "1rem",
+      }}
+      >
+    <Link to="/invoices">Invoices</Link> | {" "}
+    <Link to="expenses">Expenses</Link>
+
+    </nav>
   )
 }
