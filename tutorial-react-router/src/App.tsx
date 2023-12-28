06f1fae59cc37808453f57f34105b5393a7b4db2
auriceliof <auricelio.suporte@hotmail.com>
2023-12-28 19:31:28 -0300

    03-12 Rotas aninhadas (sub-rotas)

--- tutorial-react-router/src/App.tsx
+++ tutorial-react-router/src/App.tsx
@@ -1,18 +1,21 @@
 import './App.css'
-import { Link } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
 
 export default function App() {
 
   return (
-    <nav
-      style={{
-        borderBottom: "solid 1px",
-        paddingBottom: "1rem",
-      }}
+    <>
+      <nav
+        style={{
+          borderBottom: "solid 1px",
+          paddingBottom: "1rem",
+        }}
       >
-    <Link to="/invoices">Invoices</Link> | {" "}
-    <Link to="expenses">Expenses</Link>
+        <Link to="/invoices">Invoices</Link> | {" "}
+        <Link to="expenses">Expenses</Link>
 
-    </nav>
+      </nav>
+      <Outlet />
+    </>
   )
 }
