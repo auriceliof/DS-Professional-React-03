78e9d1d3fdd14ade4c6511161cee9882cbf8aeec
auriceliof <auricelio.suporte@hotmail.com>
2024-01-04 21:11:45 -0300

    03-17 Links ativos com NavLink

--- tutorial-react-router/src/routes/Invoices/index.tsx
+++ tutorial-react-router/src/routes/Invoices/index.tsx
@@ -1,29 +1,30 @@
-import { Link, Outlet } from "react-router-dom";
+import './styles.css';
+import { NavLink, Outlet } from "react-router-dom";
 import { getInvoices } from "../../data";
 
 export default function Invoices() {
 
-    let invoices = getInvoices();
+  let invoices = getInvoices();
 
-    return (
-        <div style={{ display: "flex" }}>
-        <nav
-          style={{
-            borderRight: "solid 1px",
-            padding: "1rem",
-          }}
-        >
-          {invoices.map((invoice) => (
-            <Link
-              style={{ display: "block", margin: "1rem 0" }}
-              to={`/invoices/${invoice.number}`}
-              key={invoice.number}
-            >
-              {invoice.name}
-            </Link>
-          ))}
-        </nav>
-        <Outlet />
-      </div>
-    );
+  return (
+    <div style={{ display: "flex" }}>
+      <nav
+        style={{
+          borderRight: "solid 1px",
+          padding: "1rem",
+        }}
+      >
+        {invoices.map((invoice) => (
+          <NavLink
+            className={({ isActive }) => isActive ? "dblock nav-red" : "dblock nav-blue"}
+            to={`/invoices/${invoice.number}`}
+            key={invoice.number}
+          >
+            {invoice.name}
+          </NavLink>
+        ))}
+      </nav>
+      <Outlet />
+    </div>
+  );
 }
