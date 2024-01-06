bdc0b447bd51dddce0a86a054be162ec6ccea0ff
auriceliof <auricelio.suporte@hotmail.com>
2024-01-06 19:43:24 -0300

    03-19 Mantendo os parâmetros de busca ao clicar outro item

--- tutorial-react-router/src/routes/Invoices/index.tsx
+++ tutorial-react-router/src/routes/Invoices/index.tsx
@@ -1,6 +1,7 @@
 import './styles.css';
 import { NavLink, Outlet, useSearchParams } from "react-router-dom";
 import { getInvoices } from "../../data";
+import QueryLink from '../../components/QueryLink';
 
 export default function Invoices() {
 
@@ -40,13 +41,13 @@ export default function Invoices() {
           })
 
           .map((invoice) => (
-            <NavLink
-              className={({ isActive }) => isActive ? "dblock nav-red" : "dblock nav-blue"}
+            <QueryLink
+              className={({ isActive }: any) => isActive ? "dblock nav-red" : "dblock nav-blue"}
               to={`/invoices/${invoice.number}`}
               key={invoice.number}
             >
               {invoice.name}
-            </NavLink>
+            </QueryLink>
           ))}
       </nav>
       <Outlet />
