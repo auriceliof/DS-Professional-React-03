ffa3f67569e3fcd97f858ac1016a7741db65b491
auriceliof <auricelio.suporte@hotmail.com>
2024-01-06 21:26:23 -0300

    03-21 De volta ao DSCommerce, adicionando rotas

--- dscommerce-frontend/src/App.tsx
+++ dscommerce-frontend/src/App.tsx
@@ -1,9 +1,20 @@
-import Catalog from './routes/Catalog';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import ClientHome from './routes/ClientHome';
+import Catalog from './routes/ClientHome/Catalog';
+import ProductDetails from './routes/ClientHome/ProductDetails';
 
-function App() {
+export default function App() {
   return (
-    <Catalog />
-  );
-}
+    <BrowserRouter>
+      <Routes>
 
-export default App
+        <Route path="/" element={<ClientHome />} >
+          <Route index element={<Catalog />} />
+          <Route path="catalog" element={<Catalog />} />
+          <Route path="product_details" element={<ProductDetails />} />
+        </Route>
+
+      </Routes>
+    </BrowserRouter>
+  )
+}
