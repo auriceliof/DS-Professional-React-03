0aae8c8d474524478f679ebc603fa6f824f51bdf
auriceliof <auricelio.suporte@hotmail.com>
2024-01-09 09:43:53 -0300

    03-24 Adicionando links nas páginas

--- dscommerce-frontend/src/routes/ClientHome/ProductDetails/index.tsx
+++ dscommerce-frontend/src/routes/ClientHome/ProductDetails/index.tsx
@@ -1,32 +1,28 @@
-import './styles.css';
+import "./styles.css";
 import ButtonInverse from "../../../components/ButtonInverse";
 import ButtonPrimary from "../../../components/ButtonPrimary";
 import ProductDetailsCard from "../../../components/ProductDetailsCard";
-import * as productService from '../../../services/product-service';
-import { useParams } from 'react-router-dom';
+import * as productService from "../../../services/product-service";
+import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function ProductDetails() {
+  const params = useParams();
 
-    const params = useParams();
+  const product = productService.findById(Number(params.productId));
 
-    const product = productService.findById(Number(params.productId));
+  return (
+    <main>
+      <section id="product-details-section" className="dsc-container">
+        {product && <ProductDetailsCard product={product} />}
 
-    return (
-
-        <main>
-            <section id="product-details-section" className="dsc-container">
-
-                {
-                    product &&
-                    <ProductDetailsCard product={product} />
-                }
-
-                <div className="dsc-btn-page-container">
-                    <ButtonPrimary text="Comprar" />
-                    <ButtonInverse text="Início" />
-                </div>
-            </section>
-        </main>
-
-    );
+        <div className="dsc-btn-page-container">
+          <ButtonPrimary text="Comprar" />
+          <Link to="/">
+            <ButtonInverse text="Início" />
+          </Link>
+        </div>
+      </section>
+    </main>
+  );
 }
