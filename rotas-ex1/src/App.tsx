f95ac6d7bdbdd8c4a2b7a59de7e8c7628b2b6c12
auriceliof <auricelio.suporte@hotmail.com>
2024-01-09 15:44:08 -0300

    03-26 Exercício 1 PARTE 1 estrutura de rotas

---
+++ rotas-ex1/src/App.tsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomeBody from "./routes/Home/HomeBody";
+import Home from "./routes/Home";
+import Promotion from "./routes/Home/Promotion";
+
+export default function App() {
+  
+  return (
+   <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />}>
+        <Route index element={<HomeBody />}/>
+        <Route path="promotion" element={<Promotion />} />
+      </Route>
+    </Routes>   
+   </BrowserRouter>
+  )
+}
